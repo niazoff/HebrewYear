8bcef354012f766a9ab0400c613b4011f54cd0d4
Natanel Niazoff <nniazoff@icloud.com>
2020-07-14 15:11:28 +0300

    Add currentHebrewYear to Calendar

--- Sources/HebrewYear/Calendar+HebrewYear.swift
+++ Sources/HebrewYear/Calendar+HebrewYear.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 public extension Calendar {
+  var currentHebrewYear: HebrewYear? { hebrewYear(for: Date()) }
+  
   func hebrewYear(for date: Date) -> HebrewYear? {
     let hebrewCalendar = self.toHebrewCalendar()
     let year = hebrewCalendar.component(.year, from: date)
