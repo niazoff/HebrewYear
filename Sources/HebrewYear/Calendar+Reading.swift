e9bf276c9ad7c757481935ac8f71b7f9c7a68478
Natanel Niazoff <nniazoff@icloud.com>
2020-09-24 14:53:19 +0300

    Add nextParshahReading

--- Sources/HebrewYear/Calendar+Reading.swift
+++ Sources/HebrewYear/Calendar+Reading.swift
@@ -7,4 +7,15 @@ public extension Calendar {
     return HebrewYear.Month(rawValue: hebrewCalendar.component(.month, from: date))
       .flatMap { YearTypeReadings.reading(for: hebrewCalendar.hebrewYear(for: date).type, month: $0, day: hebrewCalendar.component(.day, from: date), location: location) }
   }
+  
+  func nextParshahReading(after date: Date, direction: Calendar.SearchDirection = .forward, location: Location = .diaspora) -> (Date?, ParshahReading?) {
+    var nextShabbatDate: Date?
+    var reading: ParshahReading?
+    while reading == nil {
+      nextShabbatDate = self.nextShabbatDate(after: nextShabbatDate ?? date, direction: direction)
+      guard let date = nextShabbatDate else { break }
+      reading = self.reading(for: date, location: location) as? ParshahReading
+    }
+    return (nextShabbatDate, reading)
+  }
 }
