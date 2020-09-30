c9e883aefd6b0d4cd98eb3ff764b722ea34cc50e
Natanel Niazoff <nniazoff@icloud.com>
2020-09-30 12:11:41 +0300

    Change return type of nextParshahReading

--- Sources/HebrewYear/Calendar+Reading.swift
+++ Sources/HebrewYear/Calendar+Reading.swift
@@ -8,7 +8,7 @@ public extension Calendar {
       .flatMap { YearTypeReadings.reading(for: hebrewCalendar.hebrewYear(for: date).type, month: $0, day: hebrewCalendar.component(.day, from: date), location: location) }
   }
   
-  func nextParshahReading(after date: Date, direction: Calendar.SearchDirection = .forward, location: Location = .diaspora) -> (Date?, ParshahReading?) {
+  func nextParshahReading(after date: Date, direction: Calendar.SearchDirection = .forward, location: Location = .diaspora) -> (Date, ParshahReading)? {
     var nextShabbatDate: Date?
     var reading: ParshahReading?
     while reading == nil {
@@ -16,6 +16,6 @@ public extension Calendar {
       guard let date = nextShabbatDate else { break }
       reading = self.reading(for: date, location: location) as? ParshahReading
     }
-    return (nextShabbatDate, reading)
+    return nextShabbatDate.flatMap { date in reading.map { (date, $0) } }
   }
 }
